b11de9f1edc51ef31b0be617de3e79c1365a9a86
aniket-mdev <aniketsuryawanshidev@gmail.com>
2023-10-12 22:47:33 +0530

    implement a unit test for login api

--- apis/api_testing/user_api_test.go
+++ apis/api_testing/user_api_test.go
@@ -15,6 +15,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var debug_logger *log.Logger
+
+func init() {
+	log_file, err := os.Create("app.log")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	debug_logger = log.New(log_file, "DEBUG : ", log.Flags())
+
+}
+
 func TestCreaUserAPI(t *testing.T) {
 	args := []struct {
 		TestName      string
@@ -22,7 +35,8 @@ func TestCreaUserAPI(t *testing.T) {
 		ExpectedCode  int
 		ExpectedError bool
 	}{
-		{TestName: "First",
+		{
+			TestName: "First",
 			RequestBody: db.CreateNewUserParams{
 				FullName: utils.RandomString(7),
 				Email:    fmt.Sprintf("%s@gmail.com", utils.RandomString(5)),
@@ -33,7 +47,8 @@ func TestCreaUserAPI(t *testing.T) {
 			ExpectedCode:  http.StatusCreated,
 			ExpectedError: false,
 		},
-		{TestName: "Second",
+		{
+			TestName: "Second",
 			RequestBody: db.CreateNewUserParams{
 				FullName: utils.RandomString(12),
 				Email:    fmt.Sprintf("%s@gmail.com[]", utils.RandomString(8)),
@@ -44,7 +59,8 @@ func TestCreaUserAPI(t *testing.T) {
 			ExpectedCode:  http.StatusBadRequest,
 			ExpectedError: true,
 		},
-		{TestName: "Third",
+		{
+			TestName: "Third",
 			RequestBody: db.CreateNewUserParams{
 				FullName: utils.RandomString(12),
 				Email:    fmt.Sprintf("%s@gmail.com", utils.RandomString(5)),
@@ -55,7 +71,8 @@ func TestCreaUserAPI(t *testing.T) {
 			ExpectedCode:  http.StatusBadRequest,
 			ExpectedError: true,
 		},
-		{TestName: "Fourth",
+		{
+			TestName: "Fourth",
 			RequestBody: db.CreateNewUserParams{
 				FullName: utils.RandomString(12),
 				Email:    fmt.Sprintf("%s@gmail.com", utils.RandomString(4)),
@@ -80,14 +97,6 @@ func TestCreaUserAPI(t *testing.T) {
 		},
 	}
 
-	log_file, err := os.Create("app.log")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	debug_logger := log.New(log_file, "DEBUG : ", log.Flags())
-
 	// url := "http://13.233.196.149:8080/api/create-user"
 	url := "http://localhost:8080/api/create-user"
 
@@ -110,13 +119,79 @@ func TestCreaUserAPI(t *testing.T) {
 			debug_logger.Println("EXPECTED STATUS CODE : ", arg.ExpectedCode)
 			debug_logger.Println()
 
-			// if arg.ExpectedError {
-			// 	require.Error(t, err)
-			// } else {
-			// 	require.NoError(t, err)
-			// }
-
 			fmt.Println("Response err : ", err, string(response_body))
+			require.Equal(t, arg.ExpectedCode, response.StatusCode)
+
+		})
+	}
+}
+
+func TestLoginAPI(t *testing.T) {
+	args := []struct {
+		TestName      string
+		Email         string
+		Password      string
+		ExpectedCode  int
+		ExpectedError bool
+	}{
+		{
+			TestName:      "First",
+			Email:         fmt.Sprintf("%s@gmail.com", utils.RandomString(12)),
+			Password:      "user123",
+			ExpectedCode:  http.StatusNotFound,
+			ExpectedError: true,
+		},
+
+		{
+			TestName:      "Second",
+			Email:         "test-user-1@gmail.com",
+			Password:      "user123",
+			ExpectedCode:  http.StatusOK,
+			ExpectedError: false,
+		}, {
+			TestName:      "Third",
+			Email:         "",
+			Password:      "user123",
+			ExpectedCode:  http.StatusBadRequest,
+			ExpectedError: true,
+		}, {
+			TestName:      "Four",
+			Email:         fmt.Sprintf("%s@gmail.com", utils.RandomString(12)),
+			Password:      "",
+			ExpectedCode:  http.StatusBadRequest,
+			ExpectedError: true,
+		}, {
+			TestName:      "Five",
+			Email:         "test-user-1@gmail.com",
+			Password:      "2user123",
+			ExpectedCode:  http.StatusUnauthorized,
+			ExpectedError: true,
+		},
+	}
+	url := "http://localhost:8080/api/login-user?"
+	for _, arg := range args {
+		t.Run(arg.TestName, func(t *testing.T) {
+
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
+
+			q := request.URL.Query()
+
+			q.Add("email", arg.Email)
+			q.Add("password", arg.Password)
+			request.URL.RawQuery = q.Encode()
+
+			response, res_err := http.DefaultClient.Do(request)
+			require.NoError(t, res_err)
+
+			response_body, err := io.ReadAll(response.Body)
+			require.NoError(t, err)
+
+			debug_logger.Println("Request BODY : ", request)
+			debug_logger.Println("Response Code : ", response.StatusCode)
+			debug_logger.Println("Response BODY : ", string(response_body))
+			debug_logger.Println()
+
 			require.Equal(t, arg.ExpectedCode, response.StatusCode)
 
 		})
